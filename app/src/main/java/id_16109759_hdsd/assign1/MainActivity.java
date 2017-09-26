19e670b951d62ddc2a76f84b1a633f8945de1d71
SeanHeaslip <sean.j.heaslip@gmail.com>
2017-09-26 22:16:40 +0100

    Press me button with toast added.

--- app/src/main/java/id_16109759_hdsd/assign1/MainActivity.java
+++ app/src/main/java/id_16109759_hdsd/assign1/MainActivity.java
@@ -3,6 +3,7 @@ package id_16109759_hdsd.assign1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        CharSequence text = getResources().getString(R.string.mess_toast);
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(this, text, duration);
+        toast.show();
     }
 }
